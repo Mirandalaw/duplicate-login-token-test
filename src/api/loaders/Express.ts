115a8b08f84b558bc1e52bd7762923f6b148b210
JeongPark <74170593+Mirandalaw@users.noreply.github.com>
2023-07-07 23:34:43 +0900

    build : Setting Express

---
+++ src/api/loaders/Express.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import routers from "../routers";
+export default async () => {
+    const app = express();
+
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.use("/", routers);
+};
